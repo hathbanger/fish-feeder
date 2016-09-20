82856cb9ba76ef480e4d5b2f407fb35ffa12277d
hathbanger <hathbanger.ah@gmail.com>
2016-09-20 16:52:33 -0600

    clean1

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: __dirname +  '/app/index.html',
-	filename: 'index.html',
-	inject: 'body'
-});
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-	entry: [
-		'./app/index.js'
-	],
-	output: {
-		path: __dirname + '/dist',
-		filename: "index_bundle.js"
-	},
-	module: {
-		loaders: [
-			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-		]
-	},
-	plugins: [HtmlWebpackPluginConfig]
-};
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: [ 'babel' ],
+      exclude: /node_modules/,
+      include: __dirname
+    }, {
+      test: /\.css?$/,
+      loaders: [ 'style', 'raw' ],
+      include: __dirname
+    }]
+  }
+}
